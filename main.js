f81f26e11279d2f73dcc3a28b8f61003910cb45c
Behnamshs <165054650+Behnamshs@users.noreply.github.com>
2025-08-16 12:13:44 +0330

    Add files via upload

---
+++ main.js
@@ -0,0 +1,77 @@
+const slider = document.querySelector('.slider');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 1.5; // سرعت جابه‌جایی
+  slider.scrollLeft = scrollLeft - walk;
+});
+
+window.addEventListener('load', () =>{
+    const slider = document.querySelector('.slider');
+    slider.scrollLeft = slider.scrollWidth;
+});
+slider.scrollLeft = slider.scrollWidth / 2;
+
+// Sidebar toggle
+const btn3 = document.querySelector('.btn3');
+const sidebar = document.querySelector('.sidebar');
+const closeBtn = document.querySelector('.close-btn')
+btn3.addEventListener('click', () => {
+  sidebar.style.display = 'block';
+  setTimeout(() => {
+    sidebar.classList.add('open');
+  }, 10);
+})
+closeBtn.addEventListener('click', () => {
+  sidebar.classList.remove('open');
+  sidebar.addEventListener('transitionend', () => {
+    if (!sidebar.classList.contains('open')) {
+      sidebar.style.display = 'none';
+    }
+  }, { once: true });
+});
+
+const btn1 = document.querySelector('.btn1');
+btn1.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+});
+const btn2 = document.querySelector('.btn2');
+const input = btn2.querySelector('input');
+
+// وقتی روی دکمه کلیک شد
+btn2.addEventListener('click', (e) => {
+  if (!btn2.classList.contains('active')) {
+    btn2.classList.add('active');
+    input.focus();
+  }
+});
+
+// وقتی بیرون کلیک شد، ببند
+document.addEventListener('click', (e) => {
+  if (!btn2.contains(e.target)) {
+    btn2.classList.remove('active');
+    input.value = "";
+  }
+});
